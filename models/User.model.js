75808c7df98578419a5ad5bde7db8c8040c9a80a
Julia Spriggs <wildcode@julia-spriggs.fr>
2023-02-27 15:44:10 +0100

    edited user model for project

--- models/User.model.js
+++ models/User.model.js
@@ -3,17 +3,10 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    email: {
+    name: {
       type: String,
-      required: [true, 'Email is required.'],
-      unique: true,
-      lowercase: true,
-      trim: true
+      required: [true, 'name of argonaut is required'],
     },
-    password: {
-      type: String,
-      required: [true, 'Password is required.']
-    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
